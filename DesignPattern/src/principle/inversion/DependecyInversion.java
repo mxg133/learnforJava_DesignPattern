1373db637d1f56e4737e336c459b710a50ea727a
‘mxg133’ <396330646@qq.com>
2020-08-06 14:34:20 +0800

    2020-8-6

--- DesignPattern/src/principle/inversion/DependecyInversion.java
+++ DesignPattern/src/principle/inversion/DependecyInversion.java
@@ -12,16 +12,16 @@ public class DependecyInversion {
 
 class Email {
 	public String getInfo() {
-		return "�����ʼ���Ϣ: hello,world";
+		return "电子邮件信息: hello,world";
 	}
 }
 
-//���Person������Ϣ�Ĺ���
-//��ʽ1����
-//1. �򵥣��Ƚ������뵽
-//2. ������ǻ�ȡ�Ķ����� ΢�ţ����ŵȵȣ��������࣬ͬʱPeronsҲҪ������Ӧ�Ľ��շ���
-//3. ���˼·������һ������Ľӿ�IReceiver, ��ʾ������, ����Person����ӿ�IReceiver��������
-//   ��ΪEmail, WeiXin �ȵ����ڽ��յķ�Χ�����Ǹ���ʵ��IReceiver �ӿھ�ok, �������Ǿͷ���������תԭ��
+//完成Person接收消息的功能
+//方式1分析
+//1. 简单，比较容易想到
+//2. 如果我们获取的对象是 weeixxiin，短信等等，则新增类，同时Perons也要增加相应的接收方法
+//3. 解决思路：引入一个抽象的接口IReceiver, 表示接收者, 这样Person类与接口IReceiver发生依赖
+//   因为Email, WeeiiXiin 等等属于接收的范围，他们各自实现IReceiver 接口就ok, 这样我们就符号依赖倒转原则
 class Person {
 	public void receive(Email email ) {
 		System.out.println(email.getInfo());
--- DesignPattern/src/principle/inversion/my/DependecyInversion.java
+++ DesignPattern/src/principle/inversion/my/DependecyInversion.java
@@ -6,4 +6,21 @@ package principle.inversion.my;
  * @description
  */
 public class DependecyInversion {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.receive(new Email());
+    }
+}
+ class Email {
+    public String getInfo(){
+        return "电子邮件信息：hello world";
+    }
+
+}
+class Person {
+    public void receive(Email email){
+        System.out.println(email.getInfo());
+    }
+
+
 }
