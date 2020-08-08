374ace3790e6e21b7e20652ebd83b26a09dab95f
‘mxg133’ <396330646@qq.com>
2020-08-08 09:21:00 +0800

    2020-8-8

--- DesignPattern/src/_2uml/_5composition/Computer.java
+++ DesignPattern/src/_2uml/_5composition/Computer.java
@@ -1,6 +1,6 @@
 package _2uml._5composition;
 
-public class Computer {
+public class Computer extends Person {
 	private Mouse mouse = new Mouse(); //鼠标可以和computer不能分离
 	private Moniter moniter = new Moniter();//显示器可以和Computer不能分离
 	public void setMouse(Mouse mouse) {
--- DesignPattern/src/_2uml/_5composition/Person.java
+++ DesignPattern/src/_2uml/_5composition/Person.java
@@ -1,6 +1,6 @@
 package _2uml._5composition;
 
-public class Person {
+public class Person extends IDCard {
     private IDCard card; //聚合关系
     private Head head = new Head(); //组合关系
 
