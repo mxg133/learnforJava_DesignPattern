a2f951146abb68ee648cf0c58a1d291928232b9b
‘mxg133’ <396330646@qq.com>
2020-08-15 12:39:46 +0800

    2020-8-15

---
+++ _1DesignPattern/src/_22state/State.java
@@ -0,0 +1,21 @@
+package _22state;
+
+/**
+ * 状态抽象类
+ * @author Administrator
+ *
+ */
+public abstract class State {
+
+
+    // 扣除积分 - 50
+    public abstract void deductMoney();
+
+    // 是否抽中奖品
+    public abstract boolean raffle();
+
+    // 发放奖品
+    public abstract  void dispensePrize();
+
+}
+
