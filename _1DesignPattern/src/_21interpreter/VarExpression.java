33def94bbadcdaf10c7ef710c504255f938b5174
‘mxg133’ <396330646@qq.com>
2020-08-15 12:35:05 +0800

    2020-8-15

---
+++ _1DesignPattern/src/_21interpreter/VarExpression.java
@@ -0,0 +1,22 @@
+package _21interpreter;
+
+import java.util.HashMap;
+
+/**
+ * 变量的解释器
+ */
+public class VarExpression extends Expression {
+
+	private String key; // key=a,key=b,key=c
+
+	public VarExpression(String key) {
+		this.key = key;
+	}
+
+	// var 就是{a=10, b=20}
+	// interpreter 根据 变量名称，返回对应值
+	@Override
+	public int interpreter(HashMap<String, Integer> var) {
+		return var.get(this.key);
+	}
+}
